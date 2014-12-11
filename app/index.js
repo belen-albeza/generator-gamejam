cbb009c1a4d17b790c4b60347a1c5e7f3652fcae
Belén Albeza <belen.albeza@gmail.com>
2014-12-11 23:35:00 +0100

    add index, package.json and jshintrc

--- app/index.js
+++ app/index.js
@@ -2,6 +2,27 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var path = require('path');
+var _ = require('lodash');
+
+var LICENSE_DATA = {
+  'MIT': {
+    name: 'MIT',
+    url: 'http://opensource.org/licenses/MIT'
+  },
+  'CC-BY-SA-4.0': {
+    name: 'Creative Commons Attribution-ShareAlike 4.0',
+    url: 'http://creativecommons.org/licenses/by-sa/4.0/'
+  },
+  'CC-BY-NC-SA-4.0': {
+    name: 'Creative Commons Attribution-NonCommercial-ShareAlike',
+    url: 'http://creativecommons.org/licenses/by-nc-sa/4.0/'
+  },
+  'GPL-3': {
+    name: 'GPL v3.0',
+    url: 'http://www.gnu.org/licenses/lgpl-3.0-standalone.html'
+  }
+}
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -17,14 +38,40 @@ module.exports = yeoman.generators.Base.extend({
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      name: 'gameName',
+      message: 'What\'s the name of your game?',
+      default: 'Awesome game'
+    }, {
+      name: 'githubUsername',
+      message: 'What is your Github username',
+      default: 'someone'
+    }, {
+      name: 'repoName',
+      message: 'What is this repository name?',
+      default: _.last(this.destinationRoot().split(path.sep))
+    }, {
+      name: 'license',
+      message: 'Which license do you want to use',
+      type: 'list',
+      default: 0,
+      choices: ['MIT', 'CC-BY-SA-4.0', 'CC-BY-NC-SA-4.0', 'GPL-3.0']
     }];
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+      if (props.githubUsername) {
+        this.github = {
+          username: props.githubUsername,
+          repository: 'https://github.com/' + props.githubUsername +
+            '/' + props.repoName
+        };
+      }
+      else {
+        this.github = {};
+      }
+
+      this.gameName = props.gameName;
+      this.license = LICENSE_DATA[props.license] || {};
+      this.license.key = props.license;
 
       done();
     }.bind(this));
@@ -32,21 +79,19 @@ module.exports = yeoman.generators.Base.extend({
 
   writing: {
     app: function () {
-      this.fs.copy(
+      this.fs.copyTpl(
         this.templatePath('_package.json'),
-        this.destinationPath('package.json')
+        this.destinationPath('package.json'),
+        this
       );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
+      this.fs.copyTpl(
+        this.templatePath('_index.html'),
+        this.destinationPath('index.html'),
+        this
       );
     },
 
     projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
       this.fs.copy(
         this.templatePath('jshintrc'),
         this.destinationPath('.jshintrc')
@@ -56,6 +101,8 @@ module.exports = yeoman.generators.Base.extend({
 
   install: function () {
     this.installDependencies({
+      bower: false,
+      npm: true,
       skipInstall: this.options['skip-install']
     });
   }
